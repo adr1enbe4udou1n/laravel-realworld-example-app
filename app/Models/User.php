2d3f71298e084e342e50f7536b874da1fcbe2706
Adrien Beaudouin <adrien.beaudouin@outlook.com>
2021-10-22 21:49:19 +0200

    up

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,47 +2,11 @@
 
 namespace App\Models;
 
-use Database\Factories\UserFactory;
-use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\DatabaseNotification;
-use Illuminate\Notifications\DatabaseNotificationCollection;
 use Illuminate\Notifications\Notifiable;
-use Illuminate\Support\Carbon;
 use Laravel\Sanctum\HasApiTokens;
-use Laravel\Sanctum\PersonalAccessToken;
 
-/**
- * App\Models\User.
- *
- * @property int                                                   $id
- * @property string                                                $name
- * @property string                                                $email
- * @property null|Carbon                                           $email_verified_at
- * @property string                                                $password
- * @property null|string                                           $remember_token
- * @property null|Carbon                                           $created_at
- * @property null|Carbon                                           $updated_at
- * @property DatabaseNotification[]|DatabaseNotificationCollection $notifications
- * @property null|int                                              $notifications_count
- * @property Collection|PersonalAccessToken[]                      $tokens
- * @property null|int                                              $tokens_count
- *
- * @method static UserFactory factory(...$parameters)
- * @method static Builder|User newModelQuery()
- * @method static Builder|User newQuery()
- * @method static Builder|User query()
- * @method static Builder|User whereCreatedAt($value)
- * @method static Builder|User whereEmail($value)
- * @method static Builder|User whereEmailVerifiedAt($value)
- * @method static Builder|User whereId($value)
- * @method static Builder|User whereName($value)
- * @method static Builder|User wherePassword($value)
- * @method static Builder|User whereRememberToken($value)
- * @method static Builder|User whereUpdatedAt($value)
- * @mixin \Eloquent
- */
 class User extends Authenticatable
 {
     use HasApiTokens;
@@ -52,7 +16,7 @@ class User extends Authenticatable
     /**
      * The attributes that are mass assignable.
      *
-     * @var array
+     * @var string[]
      */
     protected $fillable = [
         'name',
@@ -61,7 +25,7 @@ class User extends Authenticatable
     ];
 
     /**
-     * The attributes that should be hidden for arrays.
+     * The attributes that should be hidden for serialization.
      *
      * @var array
      */
@@ -71,7 +35,7 @@ class User extends Authenticatable
     ];
 
     /**
-     * The attributes that should be cast to native types.
+     * The attributes that should be cast.
      *
      * @var array
      */
