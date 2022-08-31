51fff8c5b241572d68ca527749793bfd26ba0f6c
Adrien Beaudouin <adrien.beaudouin@outlook.com>
2022-08-31 13:41:37 +0200

    fix key length

--- config/jwt.php
+++ config/jwt.php
@@ -1,6 +1,6 @@
 <?php
 
 return [
-    'secret_key' => env('JWT_SECRET_KEY', 'super-secret-key'),
+    'secret_key' => env('JWT_SECRET_KEY', 'abcdefghijklmnopqrstuvwxyz1234567890'),
     'expire' => env('JWT_EXPIRE', '+1 hour'),
 ];
