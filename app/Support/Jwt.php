2757bb4c84a778c432649bba23099e084a98f1a7
Adrien Beaudouin <adrien.beaudouin@outlook.com>
2023-02-26 20:22:16 +0100

    use jwt configuration

--- app/Support/Jwt.php
+++ app/Support/Jwt.php
@@ -4,6 +4,8 @@ namespace App\Support;
 
 use App\Models\User;
 use DateTimeImmutable;
+use Lcobucci\Clock\SystemClock;
+use Lcobucci\JWT\Configuration;
 use Lcobucci\JWT\Encoding\CannotDecodeContent;
 use Lcobucci\JWT\Encoding\ChainedFormatter;
 use Lcobucci\JWT\Encoding\JoseEncoder;
@@ -16,45 +18,52 @@ use Lcobucci\JWT\Token\InvalidTokenStructure;
 use Lcobucci\JWT\Token\Parser;
 use Lcobucci\JWT\Token\UnsupportedHeaderFound;
 use Lcobucci\JWT\UnencryptedToken;
+use Lcobucci\JWT\Validation\Constraint\SignedWith;
+use Lcobucci\JWT\Validation\Constraint\StrictValidAt;
 use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
 
 class Jwt
 {
-    private readonly Builder $tokenBuilder;
-
-    private readonly Signer $algorithm;
-
-    private readonly Key $signingKey;
+    private Configuration $configuration;
 
     public function __construct()
     {
-        $this->tokenBuilder = (new Builder(new JoseEncoder(), ChainedFormatter::default()));
-        $this->algorithm = new Sha256();
-        $this->signingKey = InMemory::plainText(config('jwt.secret_key'));
+        $this->configuration = Configuration::forSymmetricSigner(
+            new Sha256(),
+            InMemory::plainText(config('jwt.secret_key'))
+        );
+
+        $this->configuration->setValidationConstraints(
+            new SignedWith(
+                new Sha256(),
+                InMemory::plainText(config('jwt.secret_key'))
+            ),
+            new StrictValidAt(SystemClock::fromUTC())
+        );
     }
 
     public function generate(User $user): string
     {
         $now = new DateTimeImmutable();
 
-        return $this->tokenBuilder
+        return $this->configuration->builder()
             ->issuedBy(config('app.url'))
             ->issuedAt($now)
             ->canOnlyBeUsedAfter($now)
             ->expiresAt($now->modify(config('jwt.expire')))
             ->withClaim('uid', $user->getAuthIdentifier())
             ->withClaim('email', $user->email)
-            ->getToken($this->algorithm, $this->signingKey)
+            ->getToken($this->configuration->signer(), $this->configuration->signingKey())
             ->toString();
     }
 
     public function parse(string $token): UnencryptedToken
     {
-        $parser = new Parser(new JoseEncoder());
+        $token = $this->configuration->parser()->parse($token);
 
-        try {
-            $token = $parser->parse($token);
-        } catch (CannotDecodeContent|InvalidTokenStructure|UnsupportedHeaderFound $e) {
+        $constraints = $this->configuration->validationConstraints();
+
+        if (! $this->configuration->validator()->validate($token, ...$constraints)) {
             throw new BadRequestHttpException('Invalid token !');
         }
 
